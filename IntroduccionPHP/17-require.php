4e758017b55921f8b55f928f880a1e7427a4563a
Adolfo20bm <abm2491@gmail.com>
2023-06-02 19:34:16 -0500

    Include y require

--- IntroduccionPHP/17-require.php
+++ IntroduccionPHP/17-require.php
@@ -1,8 +1,11 @@
 <?php include 'includes/header.php';
 
+// requiere cuando usas funciones criticas
+// requiere_once incluye una sola vez - va a revisar si ya fue incluido y si no lo agregara
+// include cuando quieras incluir un template
 
+require 'funciones.php';
 
-
-
+iniciarApp();
 
 include 'includes/footer.php';
