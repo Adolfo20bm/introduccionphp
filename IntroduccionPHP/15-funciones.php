f04c54c8947b56117c9208a07dfc8bb9ea130145
Adolfo20bm <abm2491@gmail.com>
2023-06-02 19:24:33 -0500

    Named Parameters en php

--- IntroduccionPHP/15-funciones.php
+++ IntroduccionPHP/15-funciones.php
@@ -2,10 +2,10 @@
 declare(strict_types=1);
 include 'includes/header.php';
 
-function sumar(int $numero1 = 0, array $numero2){
+function sumar(int $numero1,int $numero2){
     echo $numero1 + $numero2;
 }
 
-sumar(10,[]);
+sumar(numero1:10, numero2:20);
 
 include 'includes/footer.php';
